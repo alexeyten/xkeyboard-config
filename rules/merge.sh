8dc90cd661519b51b21b9c1ba2f730f064b06c91
Denis Barbier <barbier@debian.org>
2006-10-09 22:55:36 +0000

    New upstream version: xkeyboard-config 0.9

---
+++ rules/merge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEST=$1
+shift
+
+if [ -z "$HDR" ]; then
+    HDR="HDR"
+fi
+
+> $DEST
+
+for i in $*; do
+  if [ "$i" == "$HDR" ] || [ "$i" == "HDR" ]; then
+    echo >> $DEST;
+    read hdr
+    echo "$hdr" >> $DEST
+  else
+    cat $i >> $DEST;
+  fi
+done < $HDR
+
